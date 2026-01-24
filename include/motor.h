2c50660aa73b419554b99cf6de3fe95a1fd820f5
Iamsigmaeiku <evandaihongyu@gmail.com>
2026-01-24 17:38:36 +0800

    Refactor Motor class to use instance Servo object
    Replaced the global Servo object with a private member in the Motor class for better encapsulation. Updated method implementations to use the instance Servo object. Also added missing includes for ESP32Servo in motor.h and SPI in screen.h.

--- include/motor.h
+++ include/motor.h
@@ -2,6 +2,7 @@
 #define MOTOR_H
 
 #include <Arduino.h>
+#include <ESP32Servo.h>
 
 // SG90 舵機控制類
 // 接腳: VCC->5V, GND->GND, Signal->GPIO13
@@ -18,7 +19,7 @@ public:
 
 private:
   uint8_t _pin;
-  // TODO: 添加必要的成員變數
+  Servo _servo;
 };
 
 #endif
