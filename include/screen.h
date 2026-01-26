89f8a7023a03b304c8558f15f5d3fd3343b83f19
Evan Chen <evanchen0308@gmail.com>
2026-01-26 20:48:06 +0800

    LCD 撰寫
    螢幕init()成功，指紋解鎖、驗證 都OK

--- include/screen.h
+++ include/screen.h
@@ -2,33 +2,23 @@
 #define SCREEN_H
 
 #include <Arduino.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
+#include "config.h"
 
-// ILI9341 螢幕控制類
-// 接腳: CS->GPIO5, RST->GPIO22, DC->GPIO21, MOSI->GPIO23, SCK->GPIO18, MISO->GPIO19
 class Screen {
 public:
-  // 初始化
+  Screen();
   void init();
-  
-  // 顯示歡迎畫面
+
   void showWelcome();
-  
-  // 顯示等待指紋
   void showWaitingForFinger();
-  
-  // 顯示驗證成功
   void showSuccess();
-  
-  // 顯示驗證失敗
   void showFailed();
 
 private:
-  void sendCommand(uint8_t cmd);
-  void sendData(uint8_t data);
-  void setAddress(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2);
-  void fillScreen(uint16_t color);
-  void fillRect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t color);
-  void drawText(uint16_t x, uint16_t y, const char* text, uint16_t color, uint8_t size);
+  Adafruit_ILI9341 tft;
 };
 
 #endif
